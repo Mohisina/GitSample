d2f41b9e33b16ace01e92a502276f14ffe4d54a1
Shaik <mohisina.shaik@accenture.com>
2015-11-30 17:17:15 +0530

    First commit

---
+++ GitSample.java
@@ -0,0 +1,23 @@
+/**
+ *
+ */
+package de.hybris.merchandise.storefront.controllers.cms;
+
+/**
+ * @author mohisina.shaik
+ *
+ */
+public class GitSample
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(final String[] args)
+	{
+		System.out.println("Hello");
+		System.out.println("This is my new GIT sample java program");
+
+	}
+
+}
